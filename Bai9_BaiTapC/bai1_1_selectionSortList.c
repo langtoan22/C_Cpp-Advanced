12aba6cd98542ce4a64c9edb5d4b878ee471bc8e
unknown <langtoanc2@gmail.com>
2023-06-01 17:41:17 +0700

    add

--- Bai9_BaiTapC/bai1_1_selectionSortList.c
+++ Bai9_BaiTapC/bai1_1_selectionSortList.c
@@ -4,22 +4,27 @@
 //cho mang ngau nhien: uint8_t array[] = {1, 3, 5, 2, 3, 4, 5, 6, 2, 6, 9, 6, 7}
 // hay sap xep theo thu tu tu be den lon
 
+
 void sortList ( uint8_t soLuong, uint8_t array[]){
-    for(int i = 0; i < soLuong; i++){
+    for(int i = 0; i < soLuong - 1; i++){
+
+    // dung 1 bien de luu gia tri nho nhat
        int  min_index = i;
+
+       // Duyet tat ca cac phan tu dung sau phan tu hien tai va cap nhat lai gia tri nho nhat
         for(int j = i+1; j < soLuong; j++){
             if(array[j] < array [min_index]){
                 min_index = j;
             }
         }
-            if(min_index != i){
-            int temp = array [i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
-        
-            }
+
+        // doi vi tri cua gia tri thu i voi gia tri nho nhat
+          uint8_t temp = array[i];
+          array[i] = array[min_index];
+          array[min_index] = temp;
     }
 }
+
 int main(int argc, char const *argv[])
 {
     uint8_t soLuong;
@@ -31,8 +36,10 @@ int main(int argc, char const *argv[])
     for(int i = 0; i < soLuong; i++){
         scanf("%d", &array[i]);
     }
+    //goi ham sap xep
     sortList(soLuong, array);
 
+    // in ra mang sau khi sap xep
     printf("mang sau khi sap xep: \n");
     
     for(int i = 0; i < soLuong; i++){
