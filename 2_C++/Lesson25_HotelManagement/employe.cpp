0401205d5b36854c84f9dc67d7e7a31b45bb16a9
unknown <langtoanc2@gmail.com>
2023-07-30 22:21:37 +0700

    add hotelManagement

--- 2_C++/Lesson25_HotelManagement/employe.cpp
+++ 2_C++/Lesson25_HotelManagement/employe.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+typedef enum{
+    RECEPTIONIST,
+    LAUDRY_STAFF,
+    LAUGGAGE_STAFF
+}jobPosition;
+
 typedef struct {
     int day;
     int month;
@@ -12,121 +18,180 @@ typedef struct {
 
 typedef struct {
     int hour;
+    int second;
     int minute;
 } Time;
 
+typedef enum{
+    IN,
+    OUT
+}Status;
+
+typedef struct{
+    Date date;
+    Time time;
+    Status status;
+}Schedule;
 class Employee {
 private:
-    string name;
+    string Name;
     string phoneNumber;
-    string jobPosition;
-    vector<pair<Date, Time>> workSchedule;
+    jobPosition Position;
+    vector <Schedule> WorkSchedule;
 
 public:
-    Employee(string name, string phoneNumber, string jobPosition);
-    void setName(string name);
+    Employee(string EmployeeName, string EmplyeePhoneNumber, jobPosition EmployeePosition);
+    void setName(string EmployeeName);
     string getName();
-    void setPhoneNumber(string phoneNumber);
+    void setPhoneNumber(string EmplyeePhoneNumber);
     string getPhoneNumber();
-    void setJobPosition(string jobPosition);
+    void setJobPosition(jobPosition EmployeePosition);
     string getJobPosition();
-    void addWorkSchedule(Date date, Time time);
+    void addWorkSchedule(Date date, Time time, Status status);
     void displayWorkSchedule();
 };
 
-Employee::Employee(string name, string phoneNumber, string jobPosition) {
-    this->name = name;
-    this->phoneNumber = phoneNumber;
-    this->jobPosition = jobPosition;
+Employee::Employee(string EmployeeName, string EmplyeePhoneNumber, jobPosition EmployeePosition) {
+    Name = EmployeeName;
+    phoneNumber = EmplyeePhoneNumber;
+    Position = EmployeePosition;
+    
 }
 
-void Employee::setName(string name) {
-    this->name = name;
+void Employee::setName(string EmployeeName) {
+    Name = EmployeeName;
 }
 
 string Employee::getName() {
-    return this->name;
+    return  Name;
 }
 
-void Employee::setPhoneNumber(string phoneNumber) {
-    this->phoneNumber = phoneNumber;
+void Employee::setPhoneNumber(string EmplyeePhoneNumber) {
+    phoneNumber = EmplyeePhoneNumber;
 }
 
 string Employee::getPhoneNumber() {
-    return this->phoneNumber;
+    return phoneNumber;
 }
 
-void Employee::setJobPosition(string jobPosition) {
-    this->jobPosition = jobPosition;
+void Employee::setJobPosition(jobPosition EmployeePosition) {
+    Position = EmployeePosition;
 }
 
 string Employee::getJobPosition() {
-    return this->jobPosition;
+    switch (Position) {
+        case RECEPTIONIST:
+            return "Receptionist";
+        case LAUDRY_STAFF:
+            return "Laundry Staff";
+        case LAUGGAGE_STAFF:
+            return "Luggage Staff";
+        default:
+            return "Unknown";
+    }
 }
 
-void Employee::addWorkSchedule(Date date, Time time) {
-    this->workSchedule.push_back(make_pair(date, time));
+void Employee::addWorkSchedule(Date date, Time time, Status status) {
+    
+    Schedule schedule = {date, time, status};
+    WorkSchedule.push_back(schedule);
 }
 
 void Employee::displayWorkSchedule() {
-    cout << "Work schedule for employee " << this->name << ":" << endl;
-    for (auto schedule : this->workSchedule) {
-        cout << "Date: " << schedule.first.day << "/" << schedule.first.month << "/" << schedule.first.year
-             << ", Time: " << schedule.second.hour << ":" << schedule.second.minute << endl;
+    cout << "Work schedule for employee " <<Name << ":" << endl;
+    for (auto schedule : WorkSchedule) {
+        cout << "Date: " << schedule.date.day << "/" << schedule.date.month << "/" << schedule.date.year
+             << ", Time: " << schedule.time.hour << ":" << schedule.time.minute << schedule.time.second << endl;
+        if (schedule.status == IN) {
+            cout << ", Status: IN" << endl;
+        } else if (schedule.status == OUT) {
+            cout << ", Status: OUT" << endl;
+        } else {
+            cout << ", Status: Unknown" << endl;
+        }
     }
 }
 
 class EmployeeManager {
 private:
-    vector<Employee> employeeList;
+    vector<Employee> EmployeeList;
 
 public:
     void addEmployee(Employee employee);
-    void updateEmployee(string name, string phoneNumber, string jobPosition);
+    void updateEmployee(string EmployeeName, string EmplyeePhoneNumber, jobPosition EmployeePosition);
     void removeEmployee(string name);
 };
 
 void EmployeeManager::addEmployee(Employee employee) {
-    this->employeeList.push_back(employee);
+    EmployeeList.push_back(employee);
 }
 
-void EmployeeManager::updateEmployee(string name, string phoneNumber, string jobPosition) {
-    for (auto& employee : this->employeeList) {
-        if (employee.getName() == name) {
-            employee.setPhoneNumber(phoneNumber);
-            employee.setJobPosition(jobPosition);
+void EmployeeManager::updateEmployee(string EmployeeName, string EmplyeePhoneNumber, jobPosition EmployeePosition) {
+    for (auto& employee : EmployeeList) {
+        if (employee.getName() == EmployeeName) {
+            employee.setPhoneNumber(EmplyeePhoneNumber);
+            employee.setJobPosition(EmployeePosition);
             return;
         }
     }
 }
 
-void EmployeeManager::removeEmployee(string name) {
-    auto it = this->employeeList.begin();
-    while (it != this->employeeList.end()) {
-        if (it->getName() == name) {
-            it = this->employeeList.erase(it);
-        } else {
-            ++it;
+
+void EmployeeManager::removeEmployee(string EmployeeName) {
+   
+    for(auto it = EmployeeList.begin(); it != EmployeeList .end(); ++it){
+        if(it->getName() == EmployeeName){
+            it = EmployeeList.erase(it);
         }
     }
 }
 
 int main() {
+
+    // Tạo danh sách quản lý nhân viên
     EmployeeManager employeeManager;
 
-    Employee employee1("John Smith", "1234567890", "Receptionist");
-    employee1.addWorkSchedule({1, 8, 2023}, {9, 0});
-    employee1.addWorkSchedule({2, 8, 2023}, {10, 0});
-    employeeManager.addEmployee(employee1);
+    // Tạo nhân viên a và thêm lịch làm việc
+    Employee employeeA("a", "1234567890", RECEPTIONIST);
+    employeeA.addWorkSchedule({1, 8, 2023}, {9, 0}, IN);
+    employeeA.addWorkSchedule({2, 8, 2023}, {10, 0}, IN);
+    employeeA.addWorkSchedule({3, 8, 2023}, {11, 0}, OUT);
 
-    Employee employee2("Jane Doe", "0987654321", "Housekeeper");
-    employee2.addWorkSchedule({3, 8, 2023}, {8, 0});
-    employee2.addWorkSchedule({4, 8, 2023}, {12, 0});
-    employeeManager.addEmployee(employee2);
+    // Thêm nhân viên a vào danh sách quản lý nhân viên
+    employeeManager.addEmployee(employeeA);
 
-    employeeManager.updateEmployee("John Smith", "0987654321", "Manager");
+    // Tạo nhân viên b và thêm lịch làm việc
+    Employee employeeB("b", "0987654321", LAUDRY_STAFF);
+    employeeB.addWorkSchedule({1, 8, 2023}, {8, 0}, IN);
+    employeeB.addWorkSchedule({2, 8, 2023}, {9, 0}, IN);
+    employeeB.addWorkSchedule({3, 8, 2023}, {10, 0}, OUT);
 
-    employeeManager.removeEmployee("Jane Doe");
+    // Thêm nhân viên b vào danh sách quản lý nhân viên
+    employeeManager.addEmployee(employeeB);
 
+    // Hiển thị lịch làm việc của nhân viên a
+    employeeA.displayWorkSchedule();
+
+    // Hiển thị lịch làm việc của nhân viên b
+    employeeB.displayWorkSchedule();
+
+    // Thêm nhân viên b vào danh sách quản lý nhân viên
+    employeeManager.addEmployee(employeeB);
+
+    // Hiển thị lịch làm việc của nhân viên a
+    employeeA.displayWorkSchedule();
+
+    // Hiển thị lịch làm việc của nhân viên b
+    employeeB.displayWorkSchedule();
+
+    // Xóa nhân viên a khỏi danh sách quản lý nhân viên
+    employeeManager.removeEmployee("a");
+
+    // Hiển thị danh sách quản lý nhân viên sau khi xóa
+    // Chỉ còn nhân viên b trong danh sách
+    for (auto& employee : employeeManager.EmployeeList) {
+        cout << "Name: " << employee.getName() << endl;
+    }
+    
     return 0;
 }
