290b2f1c756e8bb0486a39e0408577c55a0917bb
unknown <langtoanc2@gmail.com>
2023-08-08 18:41:13 +0700

    add

---
+++ 2_C++/Lesson25_HotelManagement/Sourcee/1_mainRoom.cpp
@@ -0,0 +1,16 @@
+/*
+* File: 1_mainRoom.cpp
+* Author: Lang Van Toan
+* Date: 23/07/2023
+* Description: This is a 1_mainRoom file for program hotel management 
+*/
+#include "1_Room.h"
+
+
+int main(int argc, char const *argv[]){
+
+    Room room();
+    roommanager();
+
+    return 0;
+}
--- 2_C++/Lesson25_HotelManagement/Sourcee/2_Customer.cpp
+++ 2_C++/Lesson25_HotelManagement/Sourcee/2_Customer.cpp
@@ -1,81 +1,119 @@
-#include <iostream>
-#include <vector>
-#include <string>
+/*
+* File: 2_Customer.cpp
+* Author: Lang Van Toan
+* Date: 23/07/2023
+* Description: This is a 2_Customer file for program hotel management 
+*/
 
-using namespace std;
+#include "2_Customer.h"
 
-typedef struct {
-    int Day;
-    int Month;
-    int Year;
-}Date;
-
-typedef struct {
-    int second;
-    int minute;
-    int hour;
-}Time;
-
-typedef enum{
-    IN,
-    OUT
-}Status;
-
-typedef struct {
-    Date date;
-    Time time;
-    Status status;
-}bookHistory;
-
-class Customer {
-    private:
-        int idRoom;
-        string name;
-        string phoneNumber;
-        string address;
-        vector<bookHistory>History;
-    public:
-        Customer(string cusName, string cusPhone, string cusAddress);
-        void setName(string cusName);
-        string getName();
-        void setPhoneNumber(string cusPhone);
-        string getPhoneNumber();
-        void setAddress(string cusAddress);
-        string getAddress();
-        void addBookHistory(bookHistory bookDetails);
-        void displayBoolHistory();
-        void getInformation();
-};
-
+/*
+* Class: Customer
+*Contructor: Customer
+*Discription: This Function initializes a new instance of the customer class
+*Input:
+*    name: Name of customer
+*    phoneNumber: Number of customer
+*    address: address of customer
+*/
 Customer :: Customer(string cusName, string cusPhone, string cusAddress){
         name = cusName;
         phoneNumber = cusPhone;
         address = cusAddress;
 }
 
+/*
+* Class: Customer
+*Function: setName
+*Discription: This Function for set name of customer
+*Input: cusName
+* Output:
+*   return: None
+*/
 void Customer :: setName(string cusName){
     name = cusName;
 }
+
+/*
+* Class: Customer
+*Function: setPhoneNumber
+*Discription: This Function for set phone number of customer
+*Input: cusPhone
+* Output:
+*   return: None
+*/
 void Customer :: setPhoneNumber(string cusPhone){
     phoneNumber = cusPhone;
 }
+
+/*
+* Class: Customer
+*Function: setAddress
+*Discription: This Function for set address of customer
+*Input: cusAddress
+* Output:
+*   return: None
+*/
 void Customer :: setAddress(string cusAddress){
     address = cusAddress;
 }
+
+/*
+* Class: Customer
+*Function: getAddress
+*Discription: This Function for get address of customer
+*Input: None
+* Output:
+*   return: adress of customer
+*/
 string Customer :: getAddress(){
     return address;
 }
+
+/*
+* Class: Customer
+*Function: getPhoneNumber
+*Discription: This Function for get phone number of customer
+*Input: None
+* Output:
+*   return: phone number of customer
+*/
 string Customer :: getPhoneNumber(){
     return phoneNumber;
 }
+
+/*
+* Class: Customer
+*Function: getName
+*Discription: This Function for get name of customer
+*Input: None
+* Output:
+*   return: name of customer
+*/
 string Customer :: getName() {
     return name;
 }
 
+/*
+* Class: Customer
+*Function: addBookHistory
+*Discription: This Function for add book history of customer
+*Input: bookDetails
+* Output:
+*   return: None
+*/
 void Customer :: addBookHistory(bookHistory bookDetails) {
-        History. push_back(bookDetails);
-    }
+    History. push_back(bookDetails);
+}
 
+/*
+* Class: Customer
+*Function: displayBoolHistory
+*Discription: This Function for display book history of customer
+*Input: None
+* Output:
+*   return: book history
+*/
 void Customer ::  displayBoolHistory(){
     cout << "Booking history for customer " << name << ":" << endl;
     for (int i = 0; i < History.size(); i++) {
@@ -91,25 +129,30 @@ void Customer ::  displayBoolHistory(){
     }
 }
 
+/*
+* Class: Customer
+*Function: getInformation
+*Discription: This Function for display information of customer
+*Input: None
+* Output:
+*   return: information of customer
+*/
 void Customer :: getInformation(){
     cout << "Name customer: " << getName() << endl;
     cout << "Phone number customer: " << getPhoneNumber() << endl;
     cout << "Address customer: " << getAddress() << endl;
 }
-class CustomerManager {
-private:
-    vector<Customer> customerList;
 
-public:
-
-    void addCustomer();
-    void updateCustomer();
-    void removeCustomer();
-    void displayCustomerInfor();
-    void CustomerMenu();
-};
-
-void CustomerManager::addCustomer() {
+/*
+* Class: CustomerManagement
+*Function: addCustomer
+*Discription: This Function for add a new customer
+*Input: 
+*  customer
+* Output:
+*   return: None
+*/
+void CustomerManagement :: addCustomer() {
     string cusName;
     string cusPhone;
     string cusAddress;
@@ -118,8 +161,15 @@ void CustomerManager::addCustomer() {
     customerList.push_back(customer1);
 }
 
-
-void CustomerManager::updateCustomer() {
+/*
+* Class: CustomerManagement
+*Function: updateCustomer
+*Discription: This Function for update information off customer
+*Input: None
+* Output:
+*   return: None
+*/
+void CustomerManagement::updateCustomer() {
     string cusName, cusPhone, cusAddress;
     cout << "Enter name you need to update: ";
     cin >> cusName;
@@ -132,7 +182,15 @@ void CustomerManager::updateCustomer() {
     }
 }
 
-void CustomerManager::removeCustomer() {
+/*
+* Class: CustomerManagement
+*Function: removeCustomer
+*Discription: This Function for remove a customer
+*Input: None
+* Output:
+*   return: None
+*/
+void CustomerManagement::removeCustomer() {
     string name;
     auto it = customerList.begin();
     for(it = customerList.begin(); it != customerList.end(); ++it){
@@ -143,14 +201,30 @@ void CustomerManager::removeCustomer() {
     }
 }
 
-void CustomerManager :: displayCustomerInfor(){
+/*
+* Class: CustomerManagement
+*Function: displayCustomerInfor
+*Discription: This Function for display list customer
+*Input: None
+* Output:
+*   return: list customer
+*/
+void CustomerManagement :: displayCustomerInfor(){
     cout << "Information of customer list: " << endl;
     for(auto it : customerList){
         it.getInformation();
     }
 }
 
-void CustomerManager :: CustomerMenu(){
+/*
+* Class: CustomerManagement
+*Function: CustomerMenu
+*Discription: This Function for display list menu to management customer
+*Input: None
+* Output:
+*   return: list customer
+*/
+void CustomerManagement :: CustomerMenu(){
     int choice;
     do{
     cout << "Enter your choice\n";
@@ -184,28 +258,3 @@ void CustomerManager :: CustomerMenu(){
     }while(true);
 
 }
-
-int main(int argc, char const *argv[])
-{
-     Customer customer("John Smith", "1234567890", "123 Main St");
-
-    bookHistory history1 = {
-        { 26, 7, 2023 },
-        { 10, 0, 0 },
-        IN
-    };
-    customer.addBookHistory(history1);
-
-    bookHistory history2 = {
-        { 26, 7, 2023 },
-        { 16, 0, 0 },
-        OUT
-    };
-    customer.addBookHistory(history2);
-
-    customer.displayBoolHistory();
-
-    return 0;
-}
-
-
